68f10fd1127980bda8a114baa5fe044a10498cef
Sir-afolabi <youngmaster456@gmail.com>
2025-07-17 19:23:40 +0100

    Troubleshooting protectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Navbar from "../src/components/Sidebar.jsx";
-import Dashboard from "./components/Dashboard.jsx";
+import Navbar from "./components/dashboard/Sidebar.jsx";
+import Dashboard from "./components/dashboard/Dashboard.jsx";
+import Login from "./components/login/login.jsx";
 
 export default function App() {
   return (
-    <div className="flex h-screen">
-      <Navbar />
-      <Dashboard />
+    // <div className="flex h-screen">
+    //   <Navbar />
+    //   <Dashboard />
+    // </div>
+    <div>
+      <Login />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        commissioner: ["Commissioner", "sans-serif"],
       },
     },
   },
